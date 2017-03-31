a4b85ba9ce2b024fe6f70da34f6d63c89de98443
figox <atabordachinea@gmail.com>
2017-03-31 22:41:28 +0200

    Adding cmdlets project

---
+++ Tutoriales/CustomCmdLet/ProcCmdLet.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Management.Automation;
+
+namespace CustomCmdLet
+{
+    [Cmdlet(VerbsCommon.Get, "Proc")]
+    public class ProcCmdLet : Cmdlet
+    {
+        private int _slice;
+
+        [Parameter(Mandatory = false)]
+        public int slice
+        {
+            get { return _slice; }
+            set { _slice = value; }
+        }
+
+        protected override void ProcessRecord()
+        {
+            var procs = Process.GetProcesses();
+
+            if (_slice > 0)
+            {
+                procs = procs.Take(_slice).ToArray();
+            }
+
+            WriteObject(procs, true);
+        }
+    }
+}
